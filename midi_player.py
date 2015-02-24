89736d0de5c5973a20726e80fbb07286387125e9
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2015-02-25 07:03:58 +0800

    Nicer code.

--- midi_player.py
+++ midi_player.py
@@ -31,13 +31,21 @@ MIDI synthetizer support enabled: make sure that your synthetizer listens
 to MMC messages (in Logic Pro: menu File > Project Settings > Synchronization >
 MIDI > Listen to MMC Input).""")
 
+def out_port():
+    """
+    Return a MIDI port for output.
+    """
+    # This initialization code is from the documentation (it is required):
+    midi_out = rtmidi.Midi_Out()
+    
+    if midi_out.get_ports():
+        midi_out.open_port(0)
+    else:
+        midi_out.open_virtual_port("realtime_annotate.py")
 
-# This initialization code is from the documentation (it is required):
-midiout = rtmidi.MidiOut()
-if midiout.get_ports():
-    midiout.open_port(0)
-else:
-    midiout.open_virtual_port("realtime_annotate.py")
+    return midi_out
+
+midi_out = out_port()
 
 def send_MMC_command(command):
     """
@@ -45,7 +53,7 @@ def send_MMC_command(command):
 
     command -- value of the command (e.g. play = 2, stop = 1, etc.)
     """
-    midiout.send_message((0xf0, 0x7f, 0x7f, 0x06, command, 0xf7))
+    midi_out.send_message((0xf0, 0x7f, 0x7f, 0x06, command, 0xf7))
 
 start = lambda: send_MMC_command(2)
 stop = lambda: send_MMC_command(1)
@@ -68,7 +76,7 @@ def set_time(hours, minutes, seconds):
     fractional_seconds, seconds = math.modf(seconds)
     seconds = int(seconds)
         
-    midiout.send_message(
+    midi_out.send_message(
         bytes.fromhex("F0 7F 7F 06 44 06 01")
         +bytes([
             hours, minutes, seconds, int(round(25*fractional_seconds)), 0,
